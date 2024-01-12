c19099353bd213f63ae439cc7e67cce8e65c161d
Jelocin123 <nicolej.basse123@gmail.com>
2024-01-12 08:50:36 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'edu_locmypostion',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
